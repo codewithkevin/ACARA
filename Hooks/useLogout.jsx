4364f9cfbc2f0b4d7031a4c9e81c84a5c0630ec7
codewithkevin <oseikelvin709@gmail.com>
2022-11-29 16:48:58 +0000

    Added Logout Context called useLogout

--- Hooks/useLogout.jsx
+++ Hooks/useLogout.jsx
@@ -1,9 +1,15 @@
 import { useAuthContext } from "./useAuthContext";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
 
   const logout = () => {
-    
+    // remove user from AsyncStorage
+    AsyncStorage.removeItem("user");
+
+    dispatch({ type: "LOGOUT" });
   };
+
+  return { logout };
 };
--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { useLogout } from "./../Hooks/useLogout";
 
 const HomeScreen = () => {
+  const { logout } = useLogout();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="">Home Screen</Text>
+    <View className="flex-1 justify-center items-center">
+      <TouchableOpacity
+        onPress={handleLogout}
+        className="items-center mt-5 bg-[#009EFF] w-80 p-5 rounded-[45px]"
+      >
+        <Text className="text-white font-bold text-md">Log Out</Text>
+      </TouchableOpacity>
     </View>
   );
 };
