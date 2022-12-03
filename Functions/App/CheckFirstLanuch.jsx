eba36a432fe16c0ae70ef6379f05637b0da66c7a
codewithkevin <oseikelvin709@gmail.com>
2022-12-03 13:46:42 +0000

    Corrected Error Function

--- Functions/App/CheckFirstLanuch.jsx
+++ Functions/App/CheckFirstLanuch.jsx
@@ -1,18 +1,21 @@
-import {useState} from "react";
-import { AsyncStorage } from "@react-native-async-storage/async-storage";
+import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const CheckFirstLanch = () => {
-    const [isAppFirstLaunched, setIsAppFirstLaunched] = useState();
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = useState();
 
-    const check = async () => {
-      const appData = await AsyncStorage.getItem("isAppFirstLaunched");
-      if (appData == null) {
-        setIsAppFirstLaunched(true);
-        AsyncStorage.setItem("isAppFirstLaunched", "false");
-      } else {
-        setIsAppFirstLaunched(false);
-      }
-    };
+  console.log(isAppFirstLaunched);
 
-    return { check, isAppFirstLaunched };
-}
+  const check = async () => {
+    const appData = await AsyncStorage.getItem("isAppFirstLaunched");
+    if (appData == null) {
+      setIsAppFirstLaunched(true);
+      // AsyncStorage.removeItem("isAppFirstLaunched");
+      await AsyncStorage.setItem("isAppFirstLaunched", "false");
+    } else {
+      setIsAppFirstLaunched(false);
+    }
+  };
+
+  return { check, isAppFirstLaunched };
+};
