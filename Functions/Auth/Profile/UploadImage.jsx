5b8fdcccef75beb429d4e4b4418b81ea43d636e4
codewithkevin <oseikelvin709@gmail.com>
2022-12-13 22:02:25 +0000

    Update

---
+++ Functions/Auth/Profile/UploadImage.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { Alert } from "react-native";
+import { app, storage, auth } from "../../../config.js";
+import { getStorage, ref, uploadBytes, uploadString } from "firebase/storage";
+import * as ImagePicker from "expo-image-picker";
+
+export const uploadImage = () => {
+  const [image, setImage] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+      convertToPNG: true,
+    });
+
+    const source = { uri: result.uri };
+    console.log(source);
+    setImage(source);
+  };
+
+  const uploadingImage = async () => {
+    setUploading(true);
+
+    const imageRef = ref(storage, `profile/image`);
+
+    let img = await fetch(image.uri);
+    let blob = await img.blob();
+
+    await uploadBytes(imageRef, blob)
+      .then((snapshot) => {
+        console.log("Uploaded a blob or file!");
+      })
+      .catch((e) => {
+        alert(e);
+      });
+
+    setUploading(false);
+    Alert.alert("Photo Uploaded Successfully");
+    setImage(null);
+  };
+
+  return { image, pickImage, uploadingImage };
+};
