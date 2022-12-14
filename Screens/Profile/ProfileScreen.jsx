deeb5681e910482532e6959d4facbea5e83d66d2
codewithkevin <oseikelvin709@gmail.com>
2022-12-14 21:47:18 +0000

    update

--- Screens/Profile/ProfileScreen.jsx
+++ Screens/Profile/ProfileScreen.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-white font-bold text-2xl">ProfileScreen</Text>
     </View>
   )
 }
