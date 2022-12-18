e8cf95eec478f4e15cd8242148371458ff0741bb
codewithkevin <oseikelvin709@gmail.com>
2022-12-18 17:58:52 +0000

    Update

--- Screens/Profile/ProfileScreen.jsx
+++ Screens/Profile/ProfileScreen.jsx
@@ -1,15 +1,17 @@
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, Text, View, ScrollView } from "react-native";
 import React from "react";
 import Title from "./../../Components/Profile/Title";
 import UserProfile from "./../../Components/Profile/UserProfile";
 
 const ProfileScreen = () => {
   return (
-    <SafeAreaView className="ml-4">
+    <SafeAreaView className="ml-4 h-full">
       <Title />
-      <View className="p-5 mt-5">
-        <UserProfile />
-      </View>
+      <ScrollView>
+        <View className="p-5">
+          <UserProfile />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
